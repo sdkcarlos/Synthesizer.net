04412fc8f989ae432e0785c49545a7e9cc4d4c8a
Carlos Delgado <sdkcarlos@outlook.com>
2015-08-22 20:09:25 +0200

    Synthesizer.net
    A text-to-speech free synthesizer for Windows

---
+++ Synthesizer.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Synthesizer.net
+{
+    partial class Synthesizer
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Synthesizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 405);
+            this.Name = "Synthesizer";
+            this.Text = "Synthesizer.net";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
